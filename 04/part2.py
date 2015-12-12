abc756c04e1568b9fdd61325a27905c23caa36a8
Darren Richardson <mnestis@users.noreply.github.com>
2015-12-12 23:16:58 +0000

    day 4, part 2

---
+++ 04/part2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import hashlib
+
+def advent_coin_hash(input_string):
+    
+    x = 0
+    hash = hashlib.md5(input_string + str(x)).hexdigest()
+
+    while hash[:6] != "000000":
+        x += 1
+        hash = hashlib.md5(input_string + str(x)).hexdigest()
+
+    return x
+
+if __name__=="__main__":
+
+    input_string = "ckczppom"
+
+    print advent_coin_hash(input_string)
