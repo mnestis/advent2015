69b12e801086c66f898bebd7f59d9bacfb5133d0
Darren Richardson <mnestis@users.noreply.github.com>
2015-12-20 12:07:02 +0000

    day 20, part 2

---
+++ 20/part2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from math import sqrt
+
+def elven_post(input):
+
+    x = 0
+    while True:
+        if how_many_presents(x) > input:
+            return x
+        x += 1
+
+def how_many_presents(house_number):
+
+    divisors = set()
+
+    for x in xrange(int(sqrt(house_number)), 0, -1):
+        if house_number % x != 0:
+            continue
+        
+        if (house_number / x) < 50:
+            divisors.add(x)
+        if x < 50:
+            divisors.add(house_number/x)
+
+    return sum(divisors) * 11
+
+if __name__=="__main__":
+    
+    input = 34000000
+
+    print elven_post(input)
